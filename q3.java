c32e7daee21f5dccfbd4eed04b77adeefd69aef9
Ahmad <info.ahmadmughal@gmail.com>
2022-05-25 19:37:12 +0500

    Deleted the files that are not in use

---
+++ q3.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class q3{
+    public static int abunduntNumber( int num) {
+        int i = 1, sum = 0;
+
+        while(i<num){
+            
+            if (num%i==0) {
+                sum +=i;
+            }
+        
+        i++;
+        }
+    
+    return sum;
+    }
+    
+    public static void main(String[]args) {
+        Scanner read = new Scanner(System.in);
+
+        int num, remain;
+    
+        System.out.print("Enter a num: ");
+        num = read.nextInt();	
+    
+        remain = num;
+    
+    	int divisor = abunduntNumber(num);
+    
+        num = remain ;
+    
+    	if (divisor>num) {
+        
+        	System.out.println(num+" is an abundant num");
+    
+    	}
+    	else {
+        	System.out.println(num+" is not an abundant num");	
+    	}  
+    }
+}
